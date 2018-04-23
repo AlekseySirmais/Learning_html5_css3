66a53983fdb622d0dc057a76cbbe9190ee0486e9
AlekseySirmais <38049697+AlekseySirmais@users.noreply.github.com>
2018-04-24 00:20:05 +0300

    1
    1

---
+++ ForFirstPage.js
@@ -0,0 +1,27 @@
+    'use strict';
+
+    const  http = require('http');
+    const  fs = require('fs');
+
+    const server = http.createServer(function (request,response) { // создаем сервер, который будет отдавать то, что нужно =)
+        console.log(request.method,request.url);
+        if (request.url=='/FirstCSS.css'){
+            const  css = fs.readFileSync('FirstCSS.css','utf8');
+            response.end(css);
+        }
+        if (request.url=='/TestJS.html'){
+            const  testJS = fs.readFileSync('TestJS.html','utf8');
+            response.end(testJS); return;
+        }
+        if (request.url=='/FirstPage.html') {
+            const html = fs.readFileSync('FirstPage.html','utf8');
+            response.end(html);
+        } else response.end();
+
+// css и html он возвращает, но необходимо возвращать еще и картинку и гифку!!!
+    });
+
+    console.log('port= ',process.env.PORT);
+
+    server.listen(3000);
+    console.log('Server Started');
